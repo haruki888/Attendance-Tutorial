84f95ef30e3fc68ee3fce2b7ac02ece903c9a036
miyuki <miyuki3672@icloud.com>
2021-11-20 04:46:28 +0000

    勤怠5.5.4終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   end
   
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
       
     else
@@ -20,6 +20,6 @@ class UsersController < ApplicationController
   private
   
    def user_params
-     params.require(:user).permit(:name,:email,:password,password_confirmation)
+     params.require(:user).permit(:name,:email,:password,:password_confirmation)
    end   
 end
