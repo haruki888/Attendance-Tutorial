9786946cdcf954a5b426704f638c702bca834e97
miyuki <miyuki3672@icloud.com>
2021-11-20 12:11:31 +0000

    勤怠6.3.1終了

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,13 @@ class SessionsController < ApplicationController
   end
   
   def create
+    user = User.find_by(email:params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else
+    flash.now[:danger]= '認証に失敗しました。'
     render:new
+    end
   end  
 end
