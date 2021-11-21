41f6fc95d827d143deff6bac06efebd7fbba5ee9
miyuki <miyuki3672@icloud.com>
2021-11-21 14:12:56 +0000

    勤怠7.1.3終了

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,6 +28,10 @@ class User < ApplicationRecord
   
   def remember
     self.remember_token = User.new_token
-    update_attribute(:remember_digest,User.degest(remember_token))
+    update_attribute(:remember_digest,User.digest(remember_token))
+  end 
+  
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end  
 end  
