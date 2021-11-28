6e068bf6f287737f85db38d07e56b21a32b1d769
miyuki <miyuki3672@icloud.com>
2021-11-28 13:33:35 +0000

    Finish full-fledged-login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,7 +8,13 @@ module SessionsHelper
     user.remember
     cookies.permanent.signed[:user_id]=user.id
     cookies.permanent[:remember_token]=user.remember_token
-  end  
+  end 
+  
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
   
   def log_out
     forget(current_user)
