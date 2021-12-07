7ac59602c26a9f5a7a52cd8e9f14551ecd25113e
miyuki <miyuki3672@icloud.com>
2021-12-07 09:07:18 +0000

    Finish add-basic-info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
-  before_action:set_user,only:[:show,:edit,:update,:destroy]
-  before_action:logged_in_user,only:[:index,:show,:edit,:update,:destroy]
-  before_action:correct_user,only:[:edit,:update]
-  before_action:admin_user,only: :destroy
+  before_action:set_user,only: [:show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
+  before_action:logged_in_user,only: [:index, :edit ,:update, :destroy,:edit_basic_info, :update_basic_info]
+  before_action:correct_user,only: [:edit, :update]
+  before_action:admin_user,only: [:destroy,:edit_basic_info, :update_basic_info]
   
   
   
@@ -31,26 +31,43 @@ class UsersController < ApplicationController
   def edit
   end
   
-  def update
+ def update
     if @user.update_attributes(user_params)
-      flash[:success]="ユーザー情報を更新しました。"
+      flash[:success] = "ユーザー情報を更新しました。"
       redirect_to @user
     else
-      render:edit
+      render :edit      
     end
-  end 
-  
+  end
+
+
   def destroy
     @user.destroy
     flash[:success]="#{@user.name}のデータを削除しました。"
     redirect_to users_url
   end  
   
+  def edit_basic_info
+  end
+  
+  def update_basic_info
+    if @user.update_attributes(basic_info_params)
+    flash[:success] = "#{@user.name}の基本情報を更新しました。"
+  else
+    flash[:danger] = "#{@user.name}の更新は失敗しました。<br>" + @user.errors.full_messages.join("<br>")
+  end
+  redirect_to users_url
+end
+  
   private
   
   def user_params
     params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
   end 
+  
+  def basic_info_params
+    params.require(:user).permit(:department,:basic_time,:work_time)
+  end  
   
   def set_user
     @user=User.find(params[:id])
